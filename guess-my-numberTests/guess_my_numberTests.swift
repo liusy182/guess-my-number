fbc1af1d343cb7665168c3030c5e1f81f1fbe551
liusy182 <liusy182@gmail.com>
2016-03-25 11:04:11 +0800

    Initial Commit

---
+++ guess-my-numberTests/guess_my_numberTests.swift
@@ -0,0 +1,36 @@
+//
+//  guess_my_numberTests.swift
+//  guess-my-numberTests
+//
+//  Created by liusy182 on 25/3/16.
+//  Copyright © 2016 liusy182. All rights reserved.
+//
+
+import XCTest
+@testable import guess_my_number
+
+class guess_my_numberTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
